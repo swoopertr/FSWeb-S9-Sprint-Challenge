604a6ef307f4e69dadcfbac3507b67dbe4c9e116
sybrk <snn.yll@gmail.com>
2024-05-07 23:12:36 +0300

    css celebration animation done

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -12,25 +12,25 @@ export default function AppFunctional(props) {
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
   //const [currentIndex, setcurrentIndex]=useState(4);
 
-  const [message, setMessage]=useState(initialMessage);
-  const [email, setEmail]=useState(initialEmail);
-  const [steps, setSteps]=useState(initialSteps);
-  const [currentIndex, setcurrentIndex]=useState(initialIndex);
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [steps, setSteps] = useState(initialSteps);
+  const [currentIndex, setcurrentIndex] = useState(initialIndex);
   const [coordinates, setCoordinates] = useState({});
-  const [dimention, setDimention] = useState({rows:3,cols:5});
+  const [dimention, setDimention] = useState({ rows: 3, cols: 5 });
 
-  useEffect(()=>{
-    
-    
+  useEffect(() => {
+
+
 
     getRandFromBackend();
-    
+
     let myBody = document.getElementsByTagName("body")[0];
-    myBody.addEventListener("keydown", (evt)=> {
+    myBody.addEventListener("keydown", (evt) => {
       onKeyDown(evt)
     })
 
-  },[]);
+  }, []);
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
@@ -40,12 +40,12 @@ export default function AppFunctional(props) {
     getRandFromBackend()
   }
 
-  const getRandFromBackend = async ()=>{
+  const getRandFromBackend = async () => {
     let requestObj = {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-         },
+      },
     };
     const result = await fetch("http://localhost:9000/randDimension", requestObj);
     const resultJson = await result.json();
@@ -61,8 +61,8 @@ export default function AppFunctional(props) {
     return { y, x }
   }
 
-  function getIndexFromXY ({y, x}) {
-    const result = (((x-1) * dimention.cols + y) - 1 );
+  function getIndexFromXY({ y, x }) {
+    const result = (((x - 1) * dimention.cols + y) - 1);
     return result;
   }
 
@@ -88,41 +88,42 @@ export default function AppFunctional(props) {
         coordinates_.y = coordinates_.y - 1;
         const tempIndex = getIndexFromXY(coordinates_);
         setcurrentIndex(tempIndex);
-        setSteps(steps+1);
-       
+        setSteps(steps + 1);
+        gameWon()
+
         // backend yazılıcınca bunun comment'i kaldırılabilir.
         // checkTreasury()
       }
-    } else if (yon === "right"){
-      if(coordinates_.y === dimention.cols){
+    } else if (yon === "right") {
+      if (coordinates_.y === dimention.cols) {
         setMessage("Sağa gidemezsin")
       } else {
         coordinates_.y = coordinates_.y + 1;
         const tempIndex = getIndexFromXY(coordinates_);
         setcurrentIndex(tempIndex);
-        setSteps(steps+1);
+        setSteps(steps + 1);
         // backend yazılıcınca bunun comment'i kaldırılabilir.
         // checkTreasury()
       }
-    } else if (yon === "up"){
-      if(coordinates_.x === 1){
+    } else if (yon === "up") {
+      if (coordinates_.x === 1) {
         setMessage("Yukarı gidemezsin")
       } else {
         coordinates_.x = coordinates_.x - 1
         const tempIndex = getIndexFromXY(coordinates_);
         setcurrentIndex(tempIndex);
-        setSteps(steps+1);
+        setSteps(steps + 1);
         // backend yazılıcınca bunun comment'i kaldırılabilir.
         // checkTreasury()
       }
     } else if (yon === "down") {
-      if (coordinates_.x === dimention.rows){
+      if (coordinates_.x === dimention.rows) {
         setMessage("Asagi gidemezsin")
-      }else{
+      } else {
         coordinates_.x = coordinates_.x + 1
         const tempIndex = getIndexFromXY(coordinates_);
         setcurrentIndex(tempIndex);
-        setSteps(steps+1);
+        setSteps(steps + 1);
         // backend yazılıcınca bunun comment'i kaldırılabilir.
         // checkTreasury()
       }
@@ -136,7 +137,7 @@ export default function AppFunctional(props) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
     // burada hangi düğmeye tıklanırsa onun id'sini alıyoruz.
-    
+
     let findID = evt.target.getAttribute("id");
     // id yön belirttiği için yon parametresi alan formülü çağırıyoruz.
     sonrakiIndex(findID);
@@ -156,7 +157,7 @@ export default function AppFunctional(props) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-         },
+      },
       body: JSON.stringify({
         x: coordinates.x,
         y: coordinates.y,
@@ -167,34 +168,34 @@ export default function AppFunctional(props) {
     const result = await fetch("http://localhost:9000/api/result", requestObj);
     const resultJson = await result.json();
     setMessage(resultJson.message);
-    
+
   }
   function onKeyDown(evt) {
-    if(evt.key === "ArrowRight") {
+    if (evt.key === "ArrowRight") {
       document.getElementById('right').click();
-    } else if(evt.key === "ArrowLeft") {
+    } else if (evt.key === "ArrowLeft") {
       document.getElementById('left').click();
-    } else if(evt.key === "ArrowUp") {
+    } else if (evt.key === "ArrowUp") {
       document.getElementById('up').click();
-    } else if(evt.key === "ArrowDown") {
+    } else if (evt.key === "ArrowDown") {
       document.getElementById('down').click();
     }
   }
   async function checkTreasury(index) {
-    
+
     // backende POST request ile index gönderiyoruz.
     // gönderdiğimiz index backend'dekiyle örtüşürse oyunu bitir ve kutla, değilse false dön.
     let requestObj = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-         },
+      },
       body: JSON.stringify({
         treasuryIndex: index
       }),
     };
     const result = await fetch("http://localhost:9000/api/result", requestObj);
-    
+
     if (result) {
       //burada kutlama fonksiyonu çağırılabilir
 
@@ -204,15 +205,139 @@ export default function AppFunctional(props) {
 
   }
 
-  
+  function gameWon() {
+
+    // canvas'ı bul
+    let myCanvas = document.getElementById("canvas");
+    // canvas'ın style'ını none'den block'a çevir
+    myCanvas.style.display = "block";
+
+    // aynısını congrats h1'i için yap
+    let myh1 = document.getElementById("myh1");
+    myh1.style.display = "block";
+
+    // grid'i ortadan kaldır
+    let myGrid = document.getElementById("grid");
+    myGrid.remove(); 
+    
+    // bu fonksiyonda bundan aşağısı şuradan çalıntıdır: regards;
+    // https://codepen.io/jonathanbell/pen/OvYVYw
+    let W = window.innerWidth;
+    let H = window.innerHeight;
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const maxConfettis = 150;
+    const particles = [];
+
+    const possibleColors = [
+      "DodgerBlue",
+      "OliveDrab",
+      "Gold",
+      "Pink",
+      "SlateBlue",
+      "LightBlue",
+      "Gold",
+      "Violet",
+      "PaleGreen",
+      "SteelBlue",
+      "SandyBrown",
+      "Chocolate",
+      "Crimson"
+    ];
+
+    function randomFromTo(from, to) {
+      return Math.floor(Math.random() * (to - from + 1) + from);
+    }
+
+    function confettiParticle() {
+      this.x = Math.random() * W; // x
+      this.y = Math.random() * H - H; // y
+      this.r = randomFromTo(11, 33); // radius
+      this.d = Math.random() * maxConfettis + 11;
+      this.color =
+        possibleColors[Math.floor(Math.random() * possibleColors.length)];
+      this.tilt = Math.floor(Math.random() * 33) - 11;
+      this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+      this.tiltAngle = 0;
+
+      this.draw = function () {
+        context.beginPath();
+        context.lineWidth = this.r / 2;
+        context.strokeStyle = this.color;
+        context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+        context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+        return context.stroke();
+      };
+    }
+
+    function Draw() {
+      const results = [];
+
+      // Magical recursive functional love
+      requestAnimationFrame(Draw);
+
+      context.clearRect(0, 0, W, window.innerHeight);
+
+      for (var i = 0; i < maxConfettis; i++) {
+        results.push(particles[i].draw());
+      }
+
+      let particle = {};
+      let remainingFlakes = 0;
+      for (var i = 0; i < maxConfettis; i++) {
+        particle = particles[i];
+
+        particle.tiltAngle += particle.tiltAngleIncremental;
+        particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+        particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+        if (particle.y <= H) remainingFlakes++;
+
+        // If a confetti has fluttered out of view,
+        // bring it back to above the viewport and let if re-fall.
+        if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+          particle.x = Math.random() * W;
+          particle.y = -30;
+          particle.tilt = Math.floor(Math.random() * 10) - 20;
+        }
+      }
+
+      return results;
+    }
+
+    window.addEventListener(
+      "resize",
+      function () {
+        W = window.innerWidth;
+        H = window.innerHeight;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+      },
+      false
+    );
+
+    // Push new confetti objects to `particles[]`
+    for (var i = 0; i < maxConfettis; i++) {
+      particles.push(new confettiParticle());
+    }
+
+    // Initialize
+    canvas.width = W;
+    canvas.height = H;
+    Draw();
+
+  }
 
   return (
     <div id="wrapper" className={props.className}>
+      
+      <h1 id='myh1' className='special-h1' style={{display: "none"}}>CONGRATS!</h1>
       <div className="info">
         <h4>currentIndex : {currentIndex}</h4>
         <h3 id="coordinates">Koordinatlar {`(${Math.floor(currentIndex / dimention.cols) + 1} , ${currentIndex % dimention.cols + 1})`}</h3>
         <h3 id="steps">{steps} kere ilerlediniz</h3>
       </div>
+      <canvas id="canvas" style={{display: "none"}}></canvas>
       <div id="grid" style={
         {
           gridTemplateColumns: [...Array(dimention.cols).keys()].map(x => "100px").join(" "),
